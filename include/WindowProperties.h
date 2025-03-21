174f2ac66ef15bbe4bcf6b8f414333e39f8b8ce6
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-21 09:24:06 +0100

    ADD: [Script] WindowProperties.h

---
+++ include/WindowProperties.h
@@ -0,0 +1,13 @@
+//
+// Created by Oskar.Norberg on 2025-03-21.
+//
+
+#ifndef WINDOWPROPERTIES_H
+#define WINDOWPROPERTIES_H
+
+const char* WINDOW_TITLE = "EnTT Snake";
+
+constexpr int WINDOW_WIDTH = 720;
+constexpr int WINDOW_HEIGHT = 480;
+
+#endif //WINDOWPROPERTIES_H
--- main.cpp
+++ main.cpp
@@ -1,24 +1,13 @@
+
+#include <raylib.h>
 #include <entt.hpp>
 #include <iostream>
 
+#include "WindowProperties.h"
+
 int main()
 {
-    entt::registry registry;
-    const auto entity = registry.create();
-    registry.emplace<int>(entity, 50);
-
-    const auto entity2 = registry.create();
-    registry.emplace<int>(entity2, 50);
-
-    const auto entity3 = registry.create();
-    registry.emplace<float>(entity3);
-
-    const auto group = registry.group<int>();
-
-    for (auto entity : group)
-    {
-        std::cout << "Entity: " << (int) entity << std::endl;
-    }
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
     
     return 0;
 }
