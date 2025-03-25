bd583b555e8b14ad23fc7a863a6a719f1cf1750a
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-25 15:17:59 +0100

    ADD: [Script] Vector2

---
+++ engine/include/Vector2.h
@@ -0,0 +1,21 @@
+//
+// Created by Oskar.Norberg on 2025-03-25.
+//
+
+#ifndef VECTOR2_H
+#define VECTOR2_H
+
+namespace Core_Utils
+{
+    struct Vector2Int
+    {
+        int x, y;
+    };
+
+    struct Vector2
+    {
+        float x, y;
+    };
+}
+
+#endif //VECTOR2_H
